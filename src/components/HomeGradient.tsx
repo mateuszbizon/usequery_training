03f0725f438403a67b474733168bc87784d9521d
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-05-01 13:22:54 +0200

    added home gradient

---
+++ src/components/HomeGradient.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HomeGradient() {
+  return (
+    <div className='absolute -top-2/3 left-0 w-full h-screen bg-gradient-to-r from-primary-1 to-secondary-1 -z-10 -skew-y-[15deg]'></div>
+  )
+}
+
+export default HomeGradient
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import RootLayout from '../components/layouts/RootLayout'
+import HomeGradient from '../components/HomeGradient'
 
 function HomePage() {
   return (
     <RootLayout>
+        <HomeGradient />
         <header className='py-24'>
             <h1 className='text-4xl sm:text-5xl lg:text-7xl text-center font-medium'>DummyJSONApi <br /> reaveled <br /> with useQuery</h1>
         </header>
