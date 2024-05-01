f75017b55ed182ebe2924c32c32f1fd1b26bde5c
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-05-01 19:26:52 +0200

    changed header paragraph text color

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -8,7 +8,7 @@ function HomePage() {
         <HomeGradient />
         <header className='min-h-screen flex flex-col items-center py-24'>
             <h1 className='text-4xl sm:text-5xl lg:text-7xl text-center font-medium mb-10'>DummyJSONApi <br /> reaveled <br /> with useQuery</h1>
-            <p className='text-dark-2 mb-7 text-center sm:text-lg lg:text-xl'>In the DummyJSONApi you can get a different types of data starting with user things like users, posts, recipes, quotes and even generate images on the fly. </p>
+            <p className='text-dark-1 mb-7 text-center sm:text-lg lg:text-xl'>In the DummyJSONApi you can get a different types of data starting with user things like users, posts, recipes, quotes and even generate images on the fly. </p>
             <a href='#' className='py-1 px-4 rounded-full bg-primary-1 text-light-1 w-fit hover:bg-dark-2 transition cursor-pointer'>Start now</a>
         </header>
     </RootLayout>
