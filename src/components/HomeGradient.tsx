d9d95bbcd5b5f1fddb7f553c5a85e4aa16e7a0d1
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-05-01 19:15:12 +0200

    changed home gradient top position

--- src/components/HomeGradient.tsx
+++ src/components/HomeGradient.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function HomeGradient() {
   return (
-    <div className='absolute -top-2/3 left-0 w-full h-screen bg-gradient-to-r from-primary-1 to-secondary-1 -z-10 -skew-y-[15deg]'></div>
+    <div className='absolute -top-[350px] left-0 w-full h-screen bg-gradient-to-r from-primary-1 to-secondary-1 -z-10 -skew-y-[15deg]'></div>
   )
 }
 
