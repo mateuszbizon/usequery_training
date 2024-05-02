d61fb559d6fcd51adde62f33c8012891dabb158f
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-05-02 14:31:33 +0200

    changed a tag to Link tag

--- src/components/cards/ProductsTypesCard.tsx
+++ src/components/cards/ProductsTypesCard.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 type Props = {
     product: TProductsTypesCard;
@@ -11,7 +12,7 @@ function ProductsTypesCard({ product }: Props) {
             <img src={product.img} width={200} height={200} className='w-full h-full object-cover rounded-lg group-hover:scale-105 transition-all duration-300'></img>
         </div>
         <div className='flex justify-center items-center p-5'> 
-            <a href={product.link} className='text-center font-medium text-xl hover:text-primary-1 hover:underline transition-all duration-300'>{product.name}</a>
+            <Link to={product.link} className='text-center font-medium text-xl hover:text-primary-1 hover:underline transition-all duration-300'>{product.name}</Link>
         </div>
     </div>
   )
