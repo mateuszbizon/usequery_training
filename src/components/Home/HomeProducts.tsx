d5f485341cb52b62306b324037828d081e2178a6
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-05-02 14:35:24 +0200

    moved home components to home folder

---
+++ src/components/Home/HomeProducts.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ProductsTypesList from "../lists/ProductsTypesList";
+
+function HomeProducts() {
+	return (
+		<section id='products' className='section bg-light-2'>
+			<h2 className='text-2 text-line text-center mb-12'>Products</h2>
+			<ProductsTypesList />
+		</section>
+	);
+}
+
+export default HomeProducts;
