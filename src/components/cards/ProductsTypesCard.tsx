d69f1726515dd394f540401aacd23b5112e68a17
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-05-03 23:24:12 +0200

    changed card styles and tags

--- src/components/cards/ProductsTypesCard.tsx
+++ src/components/cards/ProductsTypesCard.tsx
@@ -6,21 +6,19 @@ type Props = {
 
 function ProductsTypesCard({ product }: Props) {
 	return (
-		<div className='group flex flex-col bg-light-1 rounded-lg max-w-[400px] mx-auto'>
-			<div className='p-2 pb-0 w-full min-h-[200px] h-full max-h-[400px]'>
+		<Link to={product.link} target="_blank" className='bg-light-1 rounded-lg h-fit'>
+			<div className='w-full h-[250px] rounded-t-lg'>
 				<img
 					src={product.img}
-					className='w-full h-full object-cover rounded-lg group-hover:scale-105 transition-all duration-300'></img>
+					className='w-full h-full object-cover'></img>
 			</div>
-			<div className='flex flex-col p-5'>
-				<Link
-					target='_blank'
-					to={product.link}
-					className='text-center font-medium text-xl hover:text-primary-1 hover:underline transition-all duration-300'>
+			<div className='p-5'>
+				<p
+					className='text-center font-medium text-xl'>
 					{product.name}
-				</Link>
+				</p>
 			</div>
-		</div>
+		</Link>
 	);
 }
 
