b66d882d5813ce7c99270948b7302b0b2ecc08d6
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-05-03 19:52:43 +0200

    changed card

--- src/components/cards/ProductsTypesCard.tsx
+++ src/components/cards/ProductsTypesCard.tsx
@@ -6,15 +6,13 @@ type Props = {
 
 function ProductsTypesCard({ product }: Props) {
 	return (
-		<div className='group flex flex-col bg-light-1 rounded-lg max-w-[400px] mx-auto h-fit'>
-			<div className='p-2 pb-0 overflow-hidden'>
+		<div className='group flex flex-col bg-light-1 rounded-lg max-w-[400px] mx-auto'>
+			<div className='p-2 pb-0 w-full min-h-[200px] h-full max-h-[400px]'>
 				<img
 					src={product.img}
-					width={200}
-					height={200}
 					className='w-full h-full object-cover rounded-lg group-hover:scale-105 transition-all duration-300'></img>
 			</div>
-			<div className='flex justify-center items-center p-5'>
+			<div className='flex flex-col p-5'>
 				<Link
 					target='_blank'
 					to={product.link}
